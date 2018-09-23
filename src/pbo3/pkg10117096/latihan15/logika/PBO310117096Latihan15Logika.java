da250c5ac7dd18765e3b578838538879425aa8dc
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:48:07 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan15/logika/PBO310117096Latihan15Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan logika
+ */
+public class PBO310117096Latihan15Logika {
+
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b =" + (a && b));
+        System.out.println("a || b =" + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) =" + !(a && b));
+    }
+    
+}
